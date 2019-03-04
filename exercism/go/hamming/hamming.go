2401b6b6b407f2021de74e44cd9335f51e0d26d9
Rohan Suri <rohan.a.suri@gmail.com>
2019-03-04 09:33:29 +0530

    refactored use of rune by comparing byte by byte

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -6,15 +6,9 @@ func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, errors.New("Hamming distance is only calculable for strands of equal length.")
 	}
-
 	distance := 0
-
-	other := []rune(b)
-
-	// range gives us runes (int32)
-	// so both strings need to be runes
-	for i, str := range a {
-		if str != other[i] {
+	for i := range a {
+		if a[i] != b[i] {
 			distance++
 		}
 	}
